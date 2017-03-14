b483b1aac0e6011132e27840f70ffc421cbf2c00
Sharykhin <siarhei.sharykhin@gmail.com>
2017-03-14 23:24:05 +0300

    Implemented command and add info into readme

--- README.md
+++ README.md
@@ -11,3 +11,21 @@ Run generating autoload file:
 ```
 php composer.phar dump-autoload
 ```
+
+Usage:
+------
+
+To get available response type use --help option:
+
+```
+php app/index.php --help
+```
+
+For running a command *--output* parameter is required:  
+There are 2 output formats available: *array* and *file*
+
+Examples:
+```
+php app/index.php --output array
+php app/index.php --output file
+```
--- app/index.php
+++ app/index.php
@@ -5,6 +5,8 @@ require __DIR__ . '/../vendor/autoload.php';
 use Graphic\Shapes\Circle;
 use Graphic\Services\DI;
 use Graphic\Calculators\ShapeCalculator;
+use Graphic\Responses\ArrayResponse;
+use Graphic\Responses\FileResponse;
 
 $shapes = [
     ['type' => 'circle', 'params' => []],
@@ -13,4 +15,36 @@ $shapes = [
 $circle = new Circle(2);
 
 $calculator = DI::resolveSetters(new ShapeCalculator($shapes));
-var_dump($calculator);
+
+$longOpts  = array(
+    "output:",
+    "help"
+);
+$cliOptions = getopt(null, $longOpts);
+if (isset($cliOptions['help'])) {
+    echo "\033[32mUsage: php index.php --output [array|file] \033[0m\n";
+    exit;
+}
+if (!isset($cliOptions['output']) ||
+    (isset($cliOptions['output']) && !in_array($cliOptions['output'], ['array', 'file']))) {
+    echo "\033[31m --output is required and must be array of file. \033[0m\n";
+    exit;
+}
+
+$response = null;
+
+switch ($cliOptions['output']) {
+    case 'array':
+        $response = new ArrayResponse($calculator);
+        break;
+    case 'file';
+        $response = new FileResponse($calculator);
+        break;
+}
+
+if (is_null($response)) {
+    echo "\033[31m something went wrong. \033[0m\n";
+    exit;
+}
+
+echo $response->output() . PHP_EOL;
